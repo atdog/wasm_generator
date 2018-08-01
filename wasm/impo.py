75c99c25de45091fce73c1b9c17aef3cdda2ca90
john <john@john-pc.local>
2018-08-01 20:25:09 +0800

    add import entry for function and memory

--- wasm/impo.py
+++ wasm/impo.py
@@ -5,5 +5,18 @@ from const import *
 def ImportSection(params):
     return ArraySection(SECTION_IMPORT, params);
 
-def ImportEntry(module, field, kind, ext):
-    return varuint(len(module)) + module + varuint(len(field)) + field + varuint(kind) + varuint(ext)
+def ImportEntry(module, field, kind, *ext):
+    prefix = varuint(len(module)) + module + varuint(len(field)) + field + varuint(kind)
+    payload = ''
+    if kind == KIND_FUNCTION:
+        payload = varuint(ext[0])
+    elif kind == KIND_MEMORY:
+        min_units = ext[0]
+        flags = 0
+        if len(ext) > 1:
+            flags = 1
+            max_units = ext[1]
+            payload = varuint(flags) + varuint(min_units) + varuint(max_units)
+        else:
+            payload = varuint(flags) + varuint(min_units)
+    return prefix + payload
